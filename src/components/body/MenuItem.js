f65c1a552ee396e2a75fabf8414edd4c87651681
Sonia <sonia798524@gmail.com>
2021-04-23 00:51:56 +0600

    DesigingMenuItem

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Card, CardTitle, CardBody, CardImg, CardImgOverlay } from 'reactstrap';
 
 
-const MenuItem = (props)=>{
+const MenuItem = (props) => {
     console.log(props);
-return(
-    <div>
-
-    </div>
-);
+    return (
+        <div>
+            <Card style={{margin:"10px"}}>
+                <CardBody>
+                    <CardImg 
+                    width="100%" 
+                    alt={props.dish.name} 
+                    src={props.dish.image}
+                    style={
+                        {opacity: "0.6"}
+                    }
+                     />
+                    <CardImgOverlay>
+                        <CardTitle>{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </Card>
+        </div>
+    );
 }
 
 
